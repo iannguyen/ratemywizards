1ccb1ff4253b4bd70f524fe8cc4b209905eb5920
Ian Nguyen <iannknguyen@gmail.com>
2015-10-20 19:17:12 -0700

    Add user picture upload feature

--- app/assets/javascripts/components/users/show.js.jsx
+++ app/assets/javascripts/components/users/show.js.jsx
@@ -3,14 +3,11 @@
 
   root.UserShow = React.createClass({
     getStateFromStore: function() {
-      console.log("getting state")
-      console.log(this.props.params.userId)
       var user  = UserStore.find(parseInt(this.props.params.userId));
       return { user: user };
     },
 
     getInitialState: function() {
-      console.log("initializing")
       return {user: {}};
     },
 
@@ -19,25 +16,14 @@
     },
 
     componentDidMount: function() {
-      console.log("mount");
       UserStore.addUserChangeListener(this._onChange);
       ApiUtil.fetchSingleUser(parseInt(this.props.params.userId));
     },
 
     componentWillUnmount: function() {
-      console.log("unmount");
       UserStore.removeUserChangeListener(this._onChange);
     },
-    componentWillUpdate: function() {
-      debugger
-      console.log("updating")
-    },
-    componentWillReceiveProps: function() {
-      console.log("receive")
-    },
-    componentDidReceiveProps: function() {
-      console.log("received")
-    },
+
     checkUserImage: function() {
       if (this.state.user.image_url === null) {
         return ("http://res.cloudinary.com/dms46o1eu/image/upload/v1445156629/Sorting_Hat_mihhnq.jpg");
@@ -48,6 +34,7 @@
 
     render: function() {
       var user = this.state.user;
+      if (user.reviews === undefined) { return <div></div>; }
       return(
         <div className="user-show">
           <div className="user-bio">
@@ -55,14 +42,16 @@
             <img className="user-pic" src={this.checkUserImage()}></img>
 
             <h1>{user.username}</h1>
-              <br/>
-            <h2>Total Reviews: {user.reviews ? user.reviews.length : 0 }</h2>
-              <br/>
 
             {window.CURRENT_USER_ID === this.state.user.id ? <CLW/> : <div></div> }
+
+              <br/>
+
+            <h2>Total Reviews: {user.reviews.length}</h2>
+
           </div>
 
-          <ReviewList reviews={user.reviews ? user.reviews : []}/>
+          <ReviewList reviews={user.reviews}/>
         </div>
       );
     }
