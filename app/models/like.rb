5acd994ff4d3a15d2d055ab38fc74d796a90298b
Ian Nguyen <iannknguyen@gmail.com>
2015-10-21 23:53:08 -0700

    austin's a god

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ActiveRecord::Base
   validates :user_id, uniqueness: { scope: :review_id}
-  validates :user_id, :review_id
+  validates :user_id, :review_id, presence: true
 
   belongs_to :review
   belongs_to :user
